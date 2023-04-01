35787685a60bafde67998ed3966a41493c5f503e
stas <point.blank.point@bk.ru>
2023-04-01 15:41:22 +0300

    t3 исправлена

--- t3/Program.cs
+++ t3/Program.cs
@@ -10,45 +10,9 @@ int ReadInt(string message)
     int value = Convert.ToInt32(Console.ReadLine());
     return value;
 }
-System.Console.WriteLine("Введите день недели");
+int number = ReadInt("Введите день недели");
 
-string Mon = "Понедельник";
-string Tue = "Вторник";
-string Wen = "Среда";
-string Thu = "Четверг";
-string Fri = "Пятница";
-string St = "Суббота";
-string Sn = "Воскресенье";
-
-var input = int.Parse(Console.ReadLine());
-
-if (input == 1)
-{
-    System.Console.WriteLine(Mon);
-}
-else if (input == 2)
-{
-    System.Console.WriteLine(Tue);
-}
-else if (input == 3)
-{
-    System.Console.WriteLine(Wen);
-}
-else if (input == 4)
-{
-    System.Console.WriteLine(Thu);
-}
-else if (input == 5)
-{
-    System.Console.WriteLine(Fri);
-}
-else if (input == 6)
-{
-    System.Console.WriteLine(St);
-}
-else if (input == 7)
-    System.Console.WriteLine(Sn);
-if (input >= 6)
+if (number >= 6)
 {
     System.Console.WriteLine($"Выходной");
 }
@@ -56,7 +20,7 @@ else
 {
     System.Console.WriteLine($"Не выходной");
 }
-if (input > 7)
+if (number > 7)
 {
     System.Console.WriteLine("Введено некоректное число");
 }
